e38fb060693b9eabb68161bef8b74a96dc43e449
CRF <yuanxin_yang@crfchina.com>
2018-11-01 11:29:35 +0800

    UIImage 分类添加方法 生成水印图片

--- ObjectiveCTools/工具包/扩展/UIImage+Image.h
+++ ObjectiveCTools/工具包/扩展/UIImage+Image.h
@@ -12,9 +12,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIImage (Image)
 
-/// 根据颜色生成图片
-+ (UIImage *)imageWithColor:(UIColor *)color;
+/// 根据颜色生成图片 参数nil 生成黑色图片
++ (UIImage *)imageWithColor:(nullable UIColor *)color;
+
+/// 给图片添加文字水印
+- (UIImage *)imageWithWaterMarkText:(nullable NSString *)text textPoint:(CGPoint)point attributedString:(nullable NSDictionary *)attributed;
+
+/// 给图片添加图片水印 参数nil返回原图片
+- (UIImage *)imageWithWaterMarkImage:(nullable UIImage *)markImage;
+
 
 @end
 
 NS_ASSUME_NONNULL_END
+
--- ObjectiveCTools/工具包/扩展/UIImage+Image.m
+++ ObjectiveCTools/工具包/扩展/UIImage+Image.m
@@ -10,31 +10,59 @@
 
 @implementation UIImage (Image)
 
-/// 根据颜色生成图片
-+ (UIImage *)imageWithColor:(UIColor *)color {
-    
+#pragma mark - 根据颜色生成图片
++ (UIImage *)imageWithColor:(nullable UIColor *)color {
+    color = nil == color ? [UIColor blackColor] : color;
     // 矩形描述
     CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
-    
     //开启位图上下文
     UIGraphicsBeginImageContext(rect.size);
-    
     //获取位图上下文
     CGContextRef context = UIGraphicsGetCurrentContext();
-    
     //使用 color 演示填充上下文
     CGContextSetFillColorWithColor(context, [color CGColor]);
-    
     //渲染上下文
     CGContextFillRect(context, rect);
-    
     // 从上下文中获取图片
     UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
-    
     //结束上下文
     UIGraphicsEndImageContext();
-    
     return theImage;
 }
 
+
+#pragma mark - 给图片添加文字水印
+- (UIImage *)imageWithWaterMarkText:(nullable NSString *)text textPoint:(CGPoint)point attributedString:(nullable NSDictionary *)attributed {
+    //1.开启上下文  尺寸 / 透明度 / 缩放
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0);
+    //2.绘制图片
+    [self drawInRect:CGRectMake(0, 0, self.size.width, self.size.height)];
+    //添加水印文字
+    [text drawAtPoint:point withAttributes:attributed];
+    //3.从上下文中获取新图片
+    UIImage * newImage = UIGraphicsGetImageFromCurrentImageContext();
+    //4.关闭图形上下文
+    UIGraphicsEndImageContext();
+    //返回图片
+    return newImage;
+}
+
+#pragma mark - 给图片添加图片水印
+- (UIImage *)imageWithWaterMarkImage:(nullable UIImage *)markImage {
+    if (!markImage) { return self; }
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0);
+    [self drawInRect:CGRectMake(0, 0, self.size.width, self.size.height)];
+    CGFloat scale = 0.3;
+    CGFloat margin = 5;
+    CGFloat waterW = markImage.size.width * scale;
+    CGFloat waterH = markImage.size.height * scale;
+    CGFloat waterX = self.size.width - waterW - margin;
+    CGFloat waterY = self.size.height - waterH - margin;
+    [markImage drawInRect:CGRectMake(waterX, waterY, waterW, waterH)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
 @end
+
